fc4acd3d1249ffd2633e749674fe3f0e15691fb0
Code Help Tutor <1823890830@qq.com>
2024-03-16 02:06:50 +0000

    WeChat cstutorcs CSE2421

---
+++ CSE2421 Lab2/libmidi/n2.h
@@ -0,0 +1,9 @@
+WeChat: cstutorcs
+QQ: 749389476
+Email: tutorcs@163.com
+/* Copyright 2022 Neil Kirby */
+
+int microsleep(unsigned int microseconds);
+int millisleep(unsigned int milliseconds);
+double now();
+
